9747613ac1f3bf591b87aa33b13d4f2fe95de175
hai.zhang <1107329694@qq.com>
2018-01-17 20:00:05 +0800

    Update RNLineChartManager.swift

--- ios/ReactNativeCharts/line/RNLineChartManager.swift
+++ ios/ReactNativeCharts/line/RNLineChartManager.swift
@@ -8,13 +8,43 @@ import UIKit
 open class RNLineChartManager: RCTViewManager, RNBarLineChartBaseManager {
   var _bridge: RCTBridge? {get{return self.bridge}}
   
+  func customDirectEventTypes() -> NSArray {
+    return ["onChange"]
+  }
+  
   override open func view() -> UIView! {
     let ins = RNLineChartView()
     return ins;
   }
-
+  
   override open static func requiresMainQueueSetup() -> Bool {
     return true;
+  }
+  
+  @objc(getValuesFromX:xValue:)
+  func getValuesFromX(_ reactTag: NSNumber, xValue: NSNumber) {
+    DispatchQueue.main.async {
+      let view: RNBarLineChartViewBase = self.bridge.uiManager.view(forReactTag: reactTag) as! RNBarLineChartViewBase;
+      var retArr = [Dictionary<String, String>]();
+      if let data = view.chart.data {
+        let dateSets = data.dataSets
+        for set in dateSets {
+          let label = set.label!
+          let count = set.entryCount;
+          for i in 0 ..< count {
+            if let entry = set.entryForIndex(i) {
+              if entry.x == xValue.doubleValue {
+                let dict = ["label": label, "X":"\(entry.x)", "Y": "\(entry.y)"]
+                retArr.append(dict)
+              }
+            }
+          }
+        }
+      }
+      
+      let dict = NSDictionary(dictionary: ["data" : retArr])
+      view.onChange?(dict as! [AnyHashable: Any])
+    }
   }
   
   func moveViewToX(_ reactTag: NSNumber, xValue: NSNumber) {
@@ -40,5 +70,16 @@ open class RNLineChartManager: RCTViewManager, RNBarLineChartBaseManager {
   func fitScreen(_ reactTag: NSNumber) {
     (self as RNBarLineChartBaseManager)._fitScreen(reactTag)
   }
+  
+}
 
+class Ret {
+  var label: String
+  var X: String
+  var Y: String
+  init(label: String, X: String, Y: String) {
+    self.label = label
+    self.X = X
+    self.Y = Y
+  }
 }
